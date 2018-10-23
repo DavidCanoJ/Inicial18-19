62ac6141d2adcda9f4a247fb496a67fe45d867ae
david <dgcj96@hotmail.com>
2018-10-23 09:08:28 +0200

    first commit

---
+++ CalcularAreaCirculo.java
@@ -0,0 +1,21 @@
+
+public class CalcularAreaCirculo {
+	
+	public static void main (String[] args) {
+		
+	//damos el radio 
+	//calcular el area
+	//(tipo double ya que es nº decimal)
+	//mostrar en consola
+	//se pueden inicializar y declarar a la vez
+	
+	double radio;
+	double area;
+	
+	radio = 7.7;
+	area  = 3.14 * radio * radio;
+	
+	System.out.println (" El área del círculo de radio " + radio  + " vale " + area );
+	}
+}
+
